a38cc509e566fcf6273f817755258e31e4522e4b
jpwmaher <jonmaher@me.com>
2018-08-29 10:05:11 +0100

    head

--- include-concept-a-head.php
+++ include-concept-a-head.php
@@ -1,13 +1,8 @@
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="format-detection" content="telephone=no">
-    <title>Long term concepts - SCRa</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="css/scra.css">
-    <link rel="stylesheet" href="css/scra-concepts.css">
-    <link rel="stylesheet" href="css/foundation-icons.css" />
-    <script src="https://www.w3schools.com/lib/w3.js"></script>
+<title>Long term concepts - SCRa</title>
+<link rel="stylesheet" href="css/foundation.css">
+<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="css/scra.css">
+<link rel="stylesheet" href="css/scra-concepts.css">
+<link rel="stylesheet" href="css/foundation-icons.css" />
+<script src="https://www.w3schools.com/lib/w3.js"></script>
 </head>
