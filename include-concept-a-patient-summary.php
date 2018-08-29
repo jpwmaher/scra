f886c2c58ea16aa79ddf17f94b2ba3607571f466
jpwmaher <jonmaher@me.com>
2018-08-29 11:24:34 +0100

    s

--- include-concept-a-patient-summary.php
+++ include-concept-a-patient-summary.php
@@ -8,16 +8,16 @@
                                 <img src="img/tiger.png" style="width: 100px;">
                             </div> -->
                         <div class="auto cell">
-                            <span class="summary-label">Full name:</span><br/><span class="summary-info summary-info-name"><?php echo $firstname  ?> <?php echo $surname  ?></span><br/>
+                            <span class="summary-label">Full name:</span><br/><span class="summary-info summary-info-name"><?php echo $firstname ?> <?php echo $surname ?></span><br/>
                             <span class="summary-info summary-info-gender-age" style="padding-top: 100px !important;">Male <span style="color: lightgray">|</span> 42 years old</span>
                         </div>
                     </div>
                 </div>
                 <div class="large-2 medium-shrink small-6 cell summary-panel">
                     <span class="summary-label">NHS No:</span><br/>
-                    <span class="summary-info"><?php echo $nhsno  ?></span><br/><br/>
+                    <span class="summary-info"><?php echo $nhsno ?></span><br/><br/>
                     <span class="summary-label summary-info-gender-age">DOB:</span><br/>
-                    <span class="summary-info"><?php echo $dob  ?></span>
+                    <span class="summary-info"><?php echo $dob ?></span>
                 </div>
                 <div id="summary-address" class="large-2 medium-shrink small-6 cell summary-panel">
                     <span class="summary-label">Address:</span><br/>
