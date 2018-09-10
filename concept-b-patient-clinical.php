7c842b9a6d28032f1ae0384413255669ebdfea52
jpwmaher <jonmaher@me.com>
2018-09-10 09:48:53 +0100

    s

--- concept-b-patient-clinical.php
+++ concept-b-patient-clinical.php
@@ -88,7 +88,7 @@
                                             <a href="#" class="accordion-title" aria-controls="lvidlv-accordion" role="tab" id="lvidlv-accordion-label" aria-expanded="false" aria-selected="false">Care Plans (1)</a>
                                             <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="lvidlv-accordion-label" aria-hidden="true" id="lvidlv-accordion">
                                                 <ul class="docs-nav-subcategory">
-                                                    <li><a href="concept-b-patient-clinical-mhcp.html#title" class="">Mental Health Crisis Plan</a></li>
+                                                    <li><a href="concept-b-patient-clinical-mhcp.php#title" class="">Mental Health Crisis Plan</a></li>
                                                 </ul>
                                             </div>
                                         </li>
@@ -106,7 +106,7 @@
                                             <div class="accordion-content is-active" data-tab-content="" role="tabpanel" aria-labelledby="yn8tgh-accordion-label" aria-hidden="true" id="yn8tgh-accordion">
                                                 <ul class="docs-nav-subcategory">
                                                     <li><a href="#">Reasonable Adjustments (2)</a></li>
-                                                    <li><a href="concept-b-patient-clinical-mhcp.html#title">Mental Health Crisis Plan</a></li>
+                                                    <li><a href="concept-b-patient-clinical-mhcp.php#title">Mental Health Crisis Plan</a></li>
                                                 </ul>
                                             </div>
                                         </li>
@@ -166,7 +166,7 @@
                                             <a href="#" class="accordion-title" aria-controls="lvidlv-accordion" role="tab" id="lvidlv-accordion-label" aria-expanded="false" aria-selected="false">Care Plans (1)</a>
                                             <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="lvidlv-accordion-label" aria-hidden="true" id="lvidlv-accordion">
                                                 <ul class="docs-nav-subcategory">
-                                                    <li><a href="concept-b-patient-clinical-mhcp.html#title" class="">Mental Health Crisis Plan</a></li>
+                                                    <li><a href="concept-b-patient-clinical-mhcp.php#title" class="">Mental Health Crisis Plan</a></li>
                                                 </ul>
                                             </div>
                                         </li>
