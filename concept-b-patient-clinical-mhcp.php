e220afd228cca9336b5aeb667ed1ced21fe16818
jpwmaher <jonmaher@me.com>
2018-09-10 10:36:36 +0100

    nav

--- concept-b-patient-clinical-mhcp.php
+++ concept-b-patient-clinical-mhcp.php
@@ -84,10 +84,10 @@
                                                 </ul>
                                             </div>
                                         </li>
-                                        <li class="accordion-item" data-accordion-item="">
+                                        <li class="accordion-item is-active" data-accordion-item="">
                                             <a href="#" class="accordion-title" aria-controls="lvidlv-accordion" role="tab" id="lvidlv-accordion-label" aria-expanded="false" aria-selected="false">Care Plans (1)</a>
-                                            <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="lvidlv-accordion-label" aria-hidden="true" id="lvidlv-accordion">
-                                                <ul class="docs-nav-subcategory">
+                                            <div class="accordion-content is-active" data-tab-content="" role="tabpanel" aria-labelledby="lvidlv-accordion-label" aria-hidden="true" id="lvidlv-accordion">
+                                                <ul class="docs-nav-subcategory" data-magellan data-deep-linking="true">
                                                     <li><a href="concept-b-patient-clinical-mhcp.php#title" class="">Mental Health Crisis Plan</a></li>
                                                 </ul>
                                             </div>
@@ -164,7 +164,7 @@
                                         <li class="accordion-item is-active" data-accordion-item="">
                                             <a href="#" class="accordion-title" aria-controls="lvidlv-accordion" role="tab" id="lvidlv-accordion-label" aria-expanded="false" aria-selected="false">Care Plans (1)</a>
                                             <div class="accordion-content is-active" data-tab-content="" role="tabpanel" aria-labelledby="lvidlv-accordion-label" aria-hidden="true" id="lvidlv-accordion">
-                                                <ul class="docs-nav-subcategory">
+                                                <ul class="docs-nav-subcategory" data-magellan data-deep-linking="true">
                                                     <li><a href="#" class="">Mental Health Crisis Plan</a></li>
                                                 </ul>
                                             </div>
