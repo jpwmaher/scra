fac19e08613e4e6b548cebcc4db7cf737bb23040
jpwmaher <jonmaher@me.com>
2018-09-12 16:00:26 +0100

    ewe

--- include-scr-gpc-tabbed-nav.php
+++ include-scr-gpc-tabbed-nav.php
@@ -5,7 +5,7 @@
                 <div class="grid-x  grid-margin-x small-up-2 medium-up-3 large-up-3">
                     <div class="cell"><a id="tab-1" class="button ghosty expanded" href="#">Summary</a></div>
                     <div class="cell"><a id="tab-2" class="button ghosty expanded" href="#">Admin</a></div>
-                    <div class="cell"><a id="tab-3" class="button ghosty expanded" href="<?php echo $link_clinical;  ?>">Clinical</a></div>
+                    <div class="cell"><a id="tab-3" class="button ghosty expanded" href="#">Clinical</a></div>
                 </div>
             </div>
         </div>
