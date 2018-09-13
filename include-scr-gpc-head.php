7ed8947c32fd5603bb20276d0c424367cbc92fa3
jpwmaher <jonmaher@me.com>
2018-09-13 11:37:44 +0100

    s

---
+++ include-scr-gpc-head.php
@@ -0,0 +1,7 @@
+<title>SCR | GPC | conflicts - v1</title>
+<link rel="stylesheet" href="css/foundation.css">
+<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="css/scra.css">
+<link rel="stylesheet" href="css/scra-concepts.css">
+<link rel="stylesheet" href="css/foundation-icons.css" />
+<script src="https://www.w3schools.com/lib/w3.js"></script>
--- scr-gpc-patient-clinical.php
+++ scr-gpc-patient-clinical.php
@@ -6,7 +6,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
-    <?php include("include-concept-b-head.php"); ?>
+    <?php include("include-scr-gpc-head.php"); ?>
 </head>
 
 <?php include("variables-concept-b.php"); ?>
