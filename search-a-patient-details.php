af4b73fceb3075b37e2f640b96e0dcb88ad3d9e2
jpwmaher <jonmaher@me.com>
2018-09-17 13:47:59 +0100

    a

--- search-a-patient-details.php
+++ search-a-patient-details.php
@@ -238,9 +238,8 @@
             </div>
         </div>
     </div>
-
     <!-- Help -->
-    <div id="privacy" class="grid-container">
+    <div id="help" class="grid-container">
         <div class="grid-x grid-padding-x">
             <div class="large-6 medium-6 cell">
                 <div class="callout ">
