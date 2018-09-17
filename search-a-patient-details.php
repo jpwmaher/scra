c0bd48a398a5d292e5feec298e6eb4b11fa04d81
jpwmaher <jonmaher@me.com>
2018-09-17 11:03:47 +0100

    a

--- search-a-patient-details.php
+++ search-a-patient-details.php
@@ -18,10 +18,9 @@
     }
 
     #tab-2 {
-        background-color: #005EB8 !important;
-        color: #fefefe;
-        font-weight: bold;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19);
+        background: #005EB8;
+        color: white;
+        border: 2px solid #005EB8;
     }
 
 </style>
