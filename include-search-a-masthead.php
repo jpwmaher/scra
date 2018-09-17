c2bb2d254b4bab8464f295d3638b936f40702736
jpwmaher <jonmaher@me.com>
2018-09-17 11:25:41 +0100

    heqd

--- include-search-a-masthead.php
+++ include-search-a-masthead.php
@@ -1,4 +1,8 @@
-<div class="cell">
-    <img src="img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;">
-    <!-- <a href="#" style="float: right; padding: 5px 0px;">New patient search</a> -->
+<div class="grid-container full">
+    <div class="grid-x grid-padding-x grid-margin-x">
+        <div class="cell">
+            <img src="img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;">
+            <!-- <a href="#" style="float: right; padding: 5px 0px;">New patient search</a> -->
+        </div>
+    </div>
 </div>
