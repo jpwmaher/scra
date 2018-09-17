f50de135867e99dae37333edd775a25ebeeb5428
jpwmaher <jonmaher@me.com>
2018-09-17 11:22:49 +0100

    search

--- include-search-a-masthead.php
+++ include-search-a-masthead.php
@@ -1,19 +1,4 @@
-    <div id="masthead-2" class="grid-container full">
-        <div id="masthead-menu" class="grid-container">
-            <div class="grid-x grid-padding-x grid-margin-x">
-                <div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="large">
-                    <!-- <button class="menu-icon" type="button" data-toggle="responsive-menu"></button> -->
-                    <div class="title-bar-title"><a href="index-403.html"><img src="/img/nhs-digital-logo-left.jpg" style="height: 20px;"> </a> &nbsp; Summary Care Record application</div>
-                </div>
-
-                <div class="top-bar" id="responsive-menu">
-                    <div class="top-bar-left">
-                        <ul class="dropdown menu show-for-large">
-                            <li><a href="index-403.html" style="padding: 0px;"><img src="/img/nhs-digital-logo-left.jpg" style="height: 40px;"></a></li>
-                            <li class="menu-text">Summary Care Record application</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="cell">
+    <img src="img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;">
+    <!-- <a href="#" style="float: right; padding: 5px 0px;">New patient search</a> -->
+</div>
