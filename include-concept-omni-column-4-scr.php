03584910fd27247f1b73cb0b1e4f41737b7f1b3e
jpwmaher <jonmaher@me.com>
2018-09-20 16:29:34 +0100

    s

--- include-concept-omni-column-4-scr.php
+++ include-concept-omni-column-4-scr.php
@@ -1,10 +1,8 @@
 <div id="fourth-column" class="cell medium-2 medium-cell-block-y content-column">
-    <div class="callout" style="font-weight: 300;  -webkit-box-shadow: 0px 0px 0px 0px rgba(65, 85, 100, 0.3);     -moz-box-shadow: 0px 0px 0px 0px rgba(65, 85, 100, 0.3);     box-shadow: 0px 0px 0px -0px rgba(65, 85, 100, 0.3);">
-        <h3 id="Disclaimer">Sourced from the patient's General Practice record. This summary may not include all the information pertinent to this patient.</h3>
-        <p id="CreateTime">Summary created: 27 May 2015 16:42</p>
-        <p id="RegistrationStartedStatement" class="HighlightedStatement">At the time this record was created, this patient had recently registered with the GP Practice. GP Summary information may not be complete.</p>
-        <p id="WithheldCREItemsHeaderStatement" class="HighlightedStatement">One or more entries have been deliberately withheld from this GP Summary.</p>
-        <p id="Author">Created by: RADFORD, Michelle (Mrs)</p>
-        <p id="Practice">DR DK NANDI'S PRACTICE, 342 Troy Road, Horsforth, Leeds LS18 5TN</p>
-    </div>
+    <h5>Sourced from the patient's General Practice record. This summary may not include all the information pertinent to this patient.</h5>
+    <p>Summary created: 27 May 2015 16:42</p>
+    <p>At the time this record was created, this patient had recently registered with the GP Practice. GP Summary information may not be complete.</p>
+    <p>One or more entries have been deliberately withheld from this GP Summary.</p>
+    <p>Created by: RADFORD, Michelle (Mrs)</p>
+    <p>DR DK NANDI'S PRACTICE, 342 Troy Road, Horsforth, Leeds LS18 5TN</p>
 </div>
