db218e10b56a61ae867bbea6e96a109abe0b8679
jpwmaher <jonmaher@me.com>
2018-09-20 14:16:00 +0100

    s

--- include-concept-omni-column-2.php
+++ include-concept-omni-column-2.php
@@ -1,7 +1,7 @@
 <div id="second-column" class="cell medium-2 medium-cell-block-y content-column">
     <ul class="menu vertical" data-deep-linking="true">
         <li><a id="col-2-link-1" href="#name">Key demographics</a>
-            <ul>
+            <ul class="menu vertical nested">
                 <li><a id="col-2-link-1" href="#name">Name</a></li>
                 <li><a id="col-2-link-2" href="#key-details">Key details</a></li>
                 <li><a id="col-2-link-3" href="#address">Address</a></li>
