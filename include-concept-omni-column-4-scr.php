08e52cc3d710509204ab59788e0b944e3cac7cd0
jpwmaher <jonmaher@me.com>
2018-09-20 16:32:51 +0100

    s

--- include-concept-omni-column-4-scr.php
+++ include-concept-omni-column-4-scr.php
@@ -1,5 +1,5 @@
 <div id="fourth-column" class="cell medium-2 medium-cell-block-y content-column">
-    <h5>Sourced from the patient's General Practice record. This summary may not include all the information pertinent to this patient.</h5>
+    <p><strong>Sourced from the patient's General Practice record. This summary may not include all the information pertinent to this patient.</strong></p>
     <p>Summary created: 27 May 2015 16:42</p>
     <p>At the time this record was created, this patient had recently registered with the GP Practice. GP Summary information may not be complete.</p>
     <p>One or more entries have been deliberately withheld from this GP Summary.</p>
