806c214eb03ff020b5fc4abce98d52e0aba8df70
jpwmaher <jonmaher@me.com>
2018-09-20 15:02:30 +0100

    s

--- include-concept-omni-column-1.php
+++ include-concept-omni-column-1.php
@@ -1,4 +1,4 @@
-<div id="first-column" class="cell medium-shrink medium-cell-block-y content-column">
+<div id="first-column" class="cell medium-shrink medium-cell-block-y content-column show-for-medium">
 
     <div class="grid-x grid-padding-x small-up-6 medium-up-1 large-up-1">
 
@@ -11,6 +11,67 @@
             </div>
         </div>
 
+        <div class="cell">
+            <div class="tab-icon">
+                <a id="vert-tab-2" href="#" class="expanded tab-icon-button button">
+                                <img src="img/pill.svg" class="tab-icon-img"><br/>
+                                <span class="tab-icon-label">Clinical</span>
+                                </a>
+            </div>
+        </div>
+
+        <div class="cell">
+            <div class="tab-icon">
+                <a id="vert-tab-3" href="#" class="expanded tab-icon-button button">
+                                <img src="img/steth.svg" class="tab-icon-img"><br/>
+                                <span class="tab-icon-label">Providers</span>
+                                </a>
+            </div>
+        </div>
+
+        <div class="cell">
+            <div class="tab-icon">
+                <a id="vert-tab-4" href="#" class="expanded tab-icon-button button">
+                                <img src="img/clock-2.svg" class="tab-icon-img"><br/>
+                                <span class="tab-icon-label">Timeline</span>
+                                </a>
+            </div>
+        </div>
+
+        <div class="cell">
+            <div class="tab-icon tab-icon-alert">
+                <a id="vert-tab-5" href="#" class="expanded tab-icon-button button">
+                                <img src="img/Alert.svg" class="tab-icon-img"><br/>
+                                <span class="tab-icon-label">                <?php echo $label_alerts ?></span>
+                                </a>
+            </div>
+        </div>
+
+        <div class="cell">
+            <div class="tab-icon tab-icon-flag">
+                <a id="vert-tab-6" href="#" class="expanded tab-icon-button button">
+                                <img src="img/flag-2.svg" class="tab-icon-img"><br/>
+                                <span class="tab-icon-label">                <?php echo $label_flags ?></span>
+                                </a>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<div id="first-column" class="cell medium-shrink medium-cell-block-y content-column hide-for-medium" style="width: 100%;">
+
+    <div class="grid-x grid-padding-x small-up-6 medium-up-1 large-up-1">
+
+        <div class="cell">
+            <div class="tab-icon tab-icon-active">
+                <a id="vert-tab-1" href="#" class="expanded tab-icon-button button">
+                                <img src="img/Torso.svg" class="tab-icon-img tab-icon-img-active"><br/>
+                                <span class="tab-icon-label">Patient</span>
+                                </a>
+            </div>
+        </div>
 
         <div class="cell">
             <div class="tab-icon">
