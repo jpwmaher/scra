6fe954ce53e26b0807f93ac0d8ca7a9e4ea83491
jpwmaher <jonmaher@me.com>
2018-09-20 16:32:20 +0100

    s

--- include-concept-omni-column-2-clinical.php
+++ include-concept-omni-column-2-clinical.php
@@ -21,6 +21,16 @@
             <a id="col-2-link-2" href="<?php echo $link_clinical_cat_2 ?>">
                 <?php echo $label_clinical_cat_2 ?>
             </a>
+            <ul class="menu vertical nested is-active">
+                <li> <a href="#second">Appointments</a> </li>
+                <li> <a href="#third">Encounters (Journal)</a> </li>
+                <li> <a href="#fourth">Clinical Items</a> </li>
+                <li> <a href="#fifth">Problems</a> </li>
+                <li> <a href="#sixth">Allergies</a> </li>
+                <li> <a href="#seventh">Medications</a> </li>
+                <li> <a href="#eighth">Referrals</a> </li>
+                <li> <a href="#ninth">Observations</a> </li>
+            </ul>
         </li>
     </ul>
 </div>
