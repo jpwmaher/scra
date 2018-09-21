f22ae45acb400c85c2f5cc2bb8b9da17d696027d
jpwmaher <jonmaher@me.com>
2018-09-21 14:10:57 +0100

    s

--- include-concept-omni-column-2-clinical-gpc.php
+++ include-concept-omni-column-2-clinical-gpc.php
@@ -4,24 +4,24 @@
             <a id="col-2-link-1" href="<?php echo $link_clinical_cat_1 ?>">
                 <?php echo $label_clinical_cat_1 ?>
             </a>
-            <ul class="menu vertical nested is-active">
-                <li><a href="#first">Allergies &amp; Sensitivities</a></li>
-                <li><a href="#second">Acute Medications</a></li>
-                <li><a href="#third">Current Repeat Medications</a></li>
-                <li><a href="#fourth">Past Repeat Medications</a></li>
-                <li><a href="#fifth">Diagnoses</a></li>
-                <li><a href="#sixth">Problems &amp; issues</a></li>
-                <li><a href="#seventh">Observations</a></li>
-                <li><a href="#eighth">Treatments</a></li>
-                <li><a href="#ninth">Results</a></li>
-                <li><a href="#tenth">Care events</a></li>
+            <ul class="menu vertical nested">
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#first">Allergies &amp; Sensitivities</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#second">Acute Medications</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#third">Current Repeat Medications</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#fourth">Past Repeat Medications</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#fifth">Diagnoses</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#sixth">Problems &amp; issues</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#seventh">Observations</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#eighth">Treatments</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#ninth">Results</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#tenth">Care events</a></li>
             </ul>
         </li>
         <li>
             <a id="col-2-link-2" href="<?php echo $link_clinical_cat_2 ?>">
                 <?php echo $label_clinical_cat_2 ?>
             </a>
-            <ul class="menu vertical nested">
+            <ul class="menu vertical nested is-active">
                 <li> <a href="<?php echo $link_clinical_cat_2 ?>#second">Appointments</a> </li>
                 <li> <a href="<?php echo $link_clinical_cat_2 ?>#third">Encounters (Journal)</a> </li>
                 <li> <a href="<?php echo $link_clinical_cat_2 ?>#fourth">Clinical Items</a> </li>
