3ab83fd96f5875cf70c5c365a7a202df9a32e84f
jpwmaher <jonmaher@me.com>
2018-09-21 14:12:55 +0100

    s

---
+++ include-concept-omni-column-4-gpc.php
@@ -0,0 +1,8 @@
+<div id="fourth-column" class="cell medium-2 medium-cell-block-y content-column">
+    <p><strong>Sourced from the patient's General Practice record. This summary may not include all the information pertinent to this patient.</strong></p>
+    <p>Summary created: 27 May 2015 16:42</p>
+    <p>At the time this record was created, this patient had recently registered with the GP Practice. GP Summary information may not be complete.</p>
+    <p>One or more entries have been deliberately withheld from this GP Summary.</p>
+    <p>Created by: RADFORD, Michelle (Mrs)</p>
+    <p>DR DK NANDI'S PRACTICE, 342 Troy Road, Horsforth, Leeds LS18 5TN</p>
+</div>
