c7afc9e9aa09766e89a4d139be8a833445359a19
jpwmaher <jonmaher@me.com>
2018-09-26 15:11:19 +0100

    s

--- include-concept-omni-column-4-timeline.php
+++ include-concept-omni-column-4-timeline.php
@@ -1,3 +1,17 @@
 <div id="fourth-column" class="cell medium-2 medium-cell-block-y content-column">
-    <span class="data-source">via GP Connect</span> <span class="small-text">on 21-Jul-2018 at 07:32</span>
+    <div class="grid-x grid-padding-x grid-margin-x">
+        <div id="timeline-filters" class="cell">
+            <div class="grid-x grid-padding-x">
+                <fieldset class="cell">
+                    <input id="checkbox1" type="checkbox" checked><label for="checkbox1">Care events</label>
+                    <input id="checkbox2" type="checkbox" checked><label for="checkbox2">Diagnoses</label>
+                    <input id="checkbox3" type="checkbox" checked><label for="checkbox3">Medications</label>
+                    <input id="checkbox4" type="checkbox" checked><label for="checkbox4">Results</label>
+                </fieldset>
+                <div class="cell" style="float: right;">
+                    <a class="button hollow tiny expanded" style="font-weight: 300;"><i class="fi-magnifying-glass"></i>&nbsp;Search timeline</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
