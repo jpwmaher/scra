eff8818089341bff092b97882a1d48c0c4677a1c
jpwmaher <jonmaher@me.com>
2018-09-26 14:49:31 +0100

    s

--- include-concept-omni-column-2-timeline.php
+++ include-concept-omni-column-2-timeline.php
@@ -2,19 +2,19 @@
     <ul class="menu vertical accordion-menu" data-deep-linking="true" data-accordion-menu data-submenu-toggle="true">
         <ul class="menu expanded vertical" data-deep-linking="true">
             <li><a href="#first"><strong>&nbsp;2018&nbsp;</strong></a>
-                <ul class="menu vertical nested">
+                <ul class="menu vertical">
                     <li><a href="#jul18">&nbsp;- July&nbsp;</a></li>
                     <li><a href="#apr18">&nbsp;- April&nbsp;</a></li>
                 </ul>
             </li>
             <li><a href="#second"><strong>&nbsp;2017&nbsp;</strong></a>
-                <ul class="menu vertical nested">
+                <ul class="menu vertical">
                     <li><a href="#nov17">&nbsp;- November&nbsp;</a></li>
                     <li><a href="#aug17">&nbsp;- August&nbsp;</a></li>
                 </ul>
             </li>
             <li><a href="#third"><strong>&nbsp;2016&nbsp;</strong></a>
-                <ul class="menu vertical nested">
+                <ul class="menu vertical">
                     <li><a href="#dec16">&nbsp;- December&nbsp;</a></li>
                     <li><a href="#nov16">&nbsp;- November&nbsp;</a></li>
                     <li><a href="#may16">&nbsp;- May&nbsp;</a></li>
