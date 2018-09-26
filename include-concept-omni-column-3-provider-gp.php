aa117c741f6b3ea8b8f48d5879f3d6f96dc02c8b
jpwmaher <jonmaher@me.com>
2018-09-26 11:32:28 +0100

    a

--- include-concept-omni-column-3-provider-gp.php
+++ include-concept-omni-column-3-provider-gp.php
@@ -1,144 +1,140 @@
 <div id="third-column" class="cell medium-auto medium-cell-block-y content-column">
 
     <h2>
-        <?php echo $label_clinical_cat_1 ?>
+        <?php echo $label_provider_cat_1 ?>
     </h2>
 
-    <div class="cell">
+    <div id="name" class="cell" data-magellan-target="name">
+        <h5>GP contact details</h5>
 
-        <br/>
-
-        <div id="name" class="cell" data-magellan-target="name">
-            <h5>GP contact details</h5>
-
-            <div class="grid-x grid-padding-x">
-                <div class="cell">
-                    <div class="grid-x grid-padding-x">
-                        <div class="medium-6 small-6 large-4 cell">
-                            <div class="panel-info-group cell">
-                                <span class="panel-label">Primary GP:</span><br/><span>Dr Matt Noble</span><br/><span>07234 567890</span><br/><span><a href="#">matt@thesurgery.co.uk</a></span><br/><span class="panel-info-note">William's GP since: 17-Apr-1969</span>
-                            </div>
-                        </div>
-                        <div class="medium-6 small-6 large-4 cell">
-                            <div class="panel-info-group cell">
-                                <span class="panel-label">Secondary GP:</span><br/><span>Dr Ryan Scanlon</span><br/><span>07234 567890</span><br/><span><a href="#">ryan@thesurgery.co.uk</a></span><br/><span class="panel-info-note">William's GP since: 17-Apr-1969</span>
-                            </div>
-                        </div>
-                        <div class="medium-6 small-6 large-4 cell">
-                            <div class="panel-info-group cell">
-                                <span class="panel-label">Locum GP:</span><br/><span>Dr Ruth Wright</span><br/><span>07234 567890</span><br/><span><a href="#">ruth@thesurgery.co.uk</a></span><br/><span class="panel-info-note">William's GP since: 17-Apr-1969</span>
-                            </div>
+        <div class="grid-x grid-padding-x">
+            <div class="cell">
+                <div class="grid-x grid-padding-x">
+                    <div class="medium-6 small-6 large-4 cell">
+                        <div class="panel-info-group cell">
+                            <span class="panel-label">Primary GP:</span><br/><span>Dr Matt Noble</span><br/><span>07234 567890</span><br/><span><a href="#">matt@thesurgery.co.uk</a></span><br/><span class="panel-info-note">William's GP since: 17-Apr-1969</span>
                         </div>
                     </div>
-                </div>
-                <div class="cell">
-                    <hr>
+                    <div class="medium-6 small-6 large-4 cell">
+                        <div class="panel-info-group cell">
+                            <span class="panel-label">Secondary GP:</span><br/><span>Dr Ryan Scanlon</span><br/><span>07234 567890</span><br/><span><a href="#">ryan@thesurgery.co.uk</a></span><br/><span class="panel-info-note">William's GP since: 17-Apr-1969</span>
+                        </div>
+                    </div>
+                    <div class="medium-6 small-6 large-4 cell">
+                        <div class="panel-info-group cell">
+                            <span class="panel-label">Locum GP:</span><br/><span>Dr Ruth Wright</span><br/><span>07234 567890</span><br/><span><a href="#">ruth@thesurgery.co.uk</a></span><br/><span class="panel-info-note">William's GP since: 17-Apr-1969</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div id="key-details" class="cell" data-magellan-target="key-details">
-            <br/><br/><br/>
-            <h5>Appointments <span style="font-size: 12px; font-weight: normal;">(in last 6 months)</span></h5>
-
-            <div id="panel-key" class="grid-x">
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Date:</span><br/><span>16-Aug-18</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
-                    </div>
-                </div>
-                <div class="medium-12 large-6 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span>
-                    </div>
-                </div>
-                <div class="cell">
-                    <hr>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Date:</span><br/><span>10-Aug-18</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
-                    </div>
-                </div>
-                <div class="medium-12 large-6 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span>
-                    </div>
-                </div>
-                <div class="cell">
-                    <hr>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Date:</span><br/><span>16-Jul-18</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
-                    </div>
-                </div>
-                <div class="medium-12 large-6 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span>
-                    </div>
-                </div>
-                <div class="cell">
-                    <hr>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Date:</span><br/><span>10-May-18</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
-                    </div>
-                </div>
-                <div class="medium-4 large-2 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
-                    </div>
-                </div>
-                <div class="medium-12 large-6 cell">
-                    <div class="panel-info-group cell">
-                        <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span><br/>
-                        <img src="img/bruise.jpg" style="max-height: 100px;">&nbsp;<img src="img/bruise-2.jpg" style="max-height: 100px;">
-                    </div>
-                </div>
-                <div class="cell">
-                    <hr>
-                </div>
+            <div class="cell">
+                <hr>
             </div>
         </div>
+    </div>
 
-        <!--   <div id="address" class="cell" data-magellan-target="address">
+    <div id="key-details" class="cell" data-magellan-target="key-details">
+        <br/><br/><br/>
+        <h5>Appointments <span style="font-size: 12px; font-weight: normal;">(in last 6 months)</span></h5>
+
+        <div id="panel-key" class="grid-x">
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Date:</span><br/><span>16-Aug-18</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
+                </div>
+            </div>
+            <div class="medium-12 large-6 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span>
+                </div>
+            </div>
+            <div class="cell">
+                <hr>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Date:</span><br/><span>10-Aug-18</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
+                </div>
+            </div>
+            <div class="medium-12 large-6 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span>
+                </div>
+            </div>
+            <div class="cell">
+                <hr>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Date:</span><br/><span>16-Jul-18</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
+                </div>
+            </div>
+            <div class="medium-12 large-6 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span>
+                </div>
+            </div>
+            <div class="cell">
+                <hr>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Date:</span><br/><span>10-May-18</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">GP visited:</span><br/><span>Dr. Matt Noble</span>
+                </div>
+            </div>
+            <div class="medium-4 large-2 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Diagnoses:</span><br/><span>n/a</span>
+                </div>
+            </div>
+            <div class="medium-12 large-6 cell">
+                <div class="panel-info-group cell">
+                    <span class="panel-label">Summary:</span><br/><span>Nullam sagittis odio urna, finibus commodo turpis porttitor in. Pellentesque id varius dui</span><br/>
+                    <img src="img/bruise.jpg" style="max-height: 100px;">&nbsp;<img src="img/bruise-2.jpg" style="max-height: 100px;">
+                </div>
+            </div>
+            <div class="cell">
+                <hr>
+            </div>
+        </div>
+    </div>
+
+    <!--   <div id="address" class="cell" data-magellan-target="address">
             <br/><br/><br/>
             <h5>Medications</h5>
 
@@ -198,38 +194,36 @@
             </div>
         </div> -->
 
-        <div id="contacts" class="cell" data-magellan-target="contacts">
-            <br/><br/><br/>
-            <h5>Admin</h5>
+    <div id="contacts" class="cell" data-magellan-target="contacts">
+        <br/><br/><br/>
+        <h5>Admin</h5>
 
-            <div class="grid-x grid-padding-x">
-                <div class="cell">
-                    <div class="grid-x  grid-margin-x small-up-2 medium-up-3 large-up-5">
-                        <div class="cell marbot"><span class="summary-label">Action:</span><br/><span><i class="fi-alert" style="color: orange;"></i> Reasonable adjustment added</span><br/></div>
-                        <div class="cell marbot"><span class="summary-label">Created by:</span><br/><span>Admin staff</span><br/></div>
-                        <div class="cell marbot"><span class="summary-label">Created on:</span><br/><span>16-Aug-18</span><br/>
-                        </div>
-                        <div class="cell marbot"><span class="summary-label">Telephone:</span><br/><span>01234 567890</span><br/></div>
-                        <div class="cell marbot"><span class="summary-label">Email: </span><br/><span>admin@thesurgery.co.uk</span><br/></div>
+        <div class="grid-x grid-padding-x">
+            <div class="cell">
+                <div class="grid-x  grid-margin-x small-up-2 medium-up-3 large-up-5">
+                    <div class="cell marbot"><span class="summary-label">Action:</span><br/><span><i class="fi-alert" style="color: orange;"></i> Reasonable adjustment added</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Created by:</span><br/><span>Admin staff</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Created on:</span><br/><span>16-Aug-18</span><br/>
                     </div>
+                    <div class="cell marbot"><span class="summary-label">Telephone:</span><br/><span>01234 567890</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Email: </span><br/><span>admin@thesurgery.co.uk</span><br/></div>
                 </div>
-                <div class="cell">
-                    <hr>
-                </div>
-                <div class="cell">
-                    <div class="grid-x  grid-margin-x small-up-2 medium-up-3 large-up-5">
-                        <div class="cell marbot"><span class="summary-label">Action:</span><br/><span>Patient email updated</span><br/></div>
-                        <div class="cell marbot"><span class="summary-label">Created by:</span><br/><span>Admin staff</span><br/></div>
-                        <div class="cell marbot"><span class="summary-label">Created on:</span><br/><span>16-Aug-18</span><br/>
-                        </div>
-                        <div class="cell marbot"><span class="summary-label">Telephone:</span><br/><span>01234 567890</span><br/></div>
-                        <div class="cell marbot"><span class="summary-label">Email: </span><br/><span>admin@thesurgery.co.uk</span><br/></div>
+            </div>
+            <div class="cell">
+                <hr>
+            </div>
+            <div class="cell">
+                <div class="grid-x  grid-margin-x small-up-2 medium-up-3 large-up-5">
+                    <div class="cell marbot"><span class="summary-label">Action:</span><br/><span>Patient email updated</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Created by:</span><br/><span>Admin staff</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Created on:</span><br/><span>16-Aug-18</span><br/>
                     </div>
+                    <div class="cell marbot"><span class="summary-label">Telephone:</span><br/><span>01234 567890</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Email: </span><br/><span>admin@thesurgery.co.uk</span><br/></div>
                 </div>
-                <div class="cell">
-                    <hr>
-                </div>
-
+            </div>
+            <div class="cell">
+                <hr>
             </div>
 
         </div>
