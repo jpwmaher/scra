516960c3489c9d162113f3f1cff72f1a3d9bf223
jpwmaher <jonmaher@me.com>
2018-09-26 15:12:44 +0100

    s

--- include-concept-omni-column-4-timeline.php
+++ include-concept-omni-column-4-timeline.php
@@ -3,12 +3,13 @@
         <div id="timeline-filters" class="cell">
             <div class="grid-x grid-padding-x">
                 <fieldset class="cell">
-                    <input id="checkbox1" type="checkbox" checked><label for="checkbox1">Care events</label>
-                    <input id="checkbox2" type="checkbox" checked><label for="checkbox2">Diagnoses</label>
-                    <input id="checkbox3" type="checkbox" checked><label for="checkbox3">Medications</label>
-                    <input id="checkbox4" type="checkbox" checked><label for="checkbox4">Results</label>
+                    <input id="checkbox1" type="checkbox" checked><label for="checkbox1">Care events</label><br/>
+                    <input id="checkbox2" type="checkbox" checked><label for="checkbox2">Diagnoses</label><br/>
+                    <input id="checkbox3" type="checkbox" checked><label for="checkbox3">Medications</label><br/>
+                    <input id="checkbox4" type="checkbox" checked><label for="checkbox4">Results</label><br/>
                 </fieldset>
                 <div class="cell" style="float: right;">
+                    <br/><br/>
                     <a class="button hollow tiny expanded" style="font-weight: 300;"><i class="fi-magnifying-glass"></i>&nbsp;Search timeline</a>
                 </div>
             </div>
