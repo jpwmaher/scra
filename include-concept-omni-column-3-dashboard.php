0a144bed251b08644eeca543f3fc0a8324a2d79c
jpwmaher <jonmaher@me.com>
2018-09-28 10:10:51 +0100

    s

--- include-concept-omni-column-3-dashboard.php
+++ include-concept-omni-column-3-dashboard.php
@@ -4,7 +4,7 @@
 
         <div class="cell">
 
-            <div class="grid-x grid-padding-x grid-margin-x small-up-1 medium-up-3 large-up-4">
+            <div class="grid-x grid-padding-x grid-margin-x small-up-1 medium-up-2 large-up-3">
                 <div class="cell dashboard-panel alert-panel">
                     <span class="panel-title">
                                     <div class="grid-x">
