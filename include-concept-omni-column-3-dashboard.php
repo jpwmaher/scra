3b5ef6b96cc80b25940d5154917eef7323a61e90
jpwmaher <jonmaher@me.com>
2018-09-28 10:16:52 +0100

    s

--- include-concept-omni-column-3-dashboard.php
+++ include-concept-omni-column-3-dashboard.php
@@ -6,6 +6,8 @@
 
         <div class="cell">
 
+            <br/>
+
             <div class="grid-x grid-padding-x grid-margin-x small-up-1 medium-up-2 large-up-3">
                 <div class="cell dashboard-panel alert-panel">
                     <span class="panel-title">
