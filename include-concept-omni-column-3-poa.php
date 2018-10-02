aa2c01513f1d2fc7e59464cc721c9389566dba71
jpwmaher <jonmaher@me.com>
2018-10-02 15:02:28 +0100

    s

--- include-concept-omni-column-3-poa.php
+++ include-concept-omni-column-3-poa.php
@@ -1,7 +1,7 @@
 <div id="third-column" class="cell medium-auto medium-cell-block-y content-column">
 
     <h2>
-        <?php echo $label_cat_3 ?> &nbsp; <img src="img/info.svg" style="height: 1.3rem;">
+        <?php echo $label_cat_3 ?> &nbsp; <a href="#" data-open="exampleModal1"><img src="img/info.svg" style="height: 1.3rem;"></a>
     </h2>
 
     <div id="poa" class="cell" data-magellan-target="poa">
@@ -47,4 +47,29 @@
         </div>
     </div>
 
+</div>
+
+<div class="reveal" id="exampleModal1" data-reveal>
+    <div id="poa-info" class="" data-toggler=".hide">
+        <div class="grid-x grid-padding-x">
+            <div class="cell callout success">
+                <h6>What is Power of Attorney?</h6>
+                <p>A lasting power of attorney (LPA) is a legal document that lets you (the ‘donor’) appoint one or more people (known as ‘attorneys’) to help you make decisions or to make decisions on your behalf.</p>
+                <p>This gives you more control over what happens to you if you have an accident or an illness and can’t make your own decisions (you ‘lack mental capacity’).</p>
+                <br/>
+                <h6>Health &amp; Welfare Power of Attorney</h6>
+                <p>A Health &amp; Welfare POA make decisions about things like:</p>
+                <ul style="list-style-type: circle;">
+                    <li>your daily routine, for example washing, dressing, eating</li>
+                    <li>medical care</li>
+                    <li>moving into a care home</li>
+                    <li>life-sustaining treatment</li>
+                </ul>
+            </div>
+            <hr>
+        </div>
+    </div>
+    <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
 </div>
