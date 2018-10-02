a90c0dff43009481b947f31029cda5995b389f57
jpwmaher <jonmaher@me.com>
2018-10-02 15:32:44 +0100

    s

--- include-concept-omni-column-3-poa.php
+++ include-concept-omni-column-3-poa.php
@@ -44,6 +44,19 @@
                     <div class="cell marbot"><span class="summary-label">Email: </span><br/><span>tez@thebard.com</span><br/></div>
                 </div>
             </div>
+            <div class="cell">
+                <hr>
+            </div>
+            <div class="cell">
+                <div class="grid-x  grid-margin-x small-up-2 medium-up-3 large-up-5">
+                    <div class="cell marbot"><span class="summary-label">Name:</span><br/><span>Terry Hathaway</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Relationship:</span><br/><span>Brother</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Address:</span><br/><span><a target="_blank"  href="https://goo.gl/maps/eWABxM6Y8Ut">101 Other Street<br/>Stratford-upon-Avon<br/>Warwickshire<br/>AB12 3DE</a></span><br/>
+                    </div>
+                    <div class="cell marbot"><span class="summary-label">Telephone:</span><br/><span>01234 567890</span><br/></div>
+                    <div class="cell marbot"><span class="summary-label">Email: </span><br/><span>tez@thebard.com</span><br/></div>
+                </div>
+            </div>
         </div>
     </div>
 
