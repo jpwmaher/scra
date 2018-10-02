663ce3ed07570c65cfda60ce542e0f8c118b375d
jpwmaher <jonmaher@me.com>
2018-10-02 11:17:19 +0100

    s

--- include-concept-omni-column-4.php
+++ include-concept-omni-column-4.php
@@ -3,7 +3,7 @@
         <h5>Information</h5>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacus odio, accumsan id ullamcorper eget, varius nec erat. Nulla facilisi. Donec dui felis, euismod nec finibus vitae, dapibus quis arcu. Maecenas tempor et ipsum quis venenatis. Ut posuere sed augue sit amet efficitur. Sed imperdiet, justo id tempus rhoncus, est est viverra turpis, non vulputate magna lectus et nisl.</p>
     </div>
-    <div class="callout" id="panel" data-toggler data-animate="hinge-in-from-top spin-out">
+    <div class="callout address-history hide" id="panel" data-toggler=".hide" data-animate="hinge-in-from-top spin-out">
         <h4>Hello!</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quas optio alias voluptas nobis iusto mollitia asperiores incidunt reprehenderit doloribus voluptatibus officiis minus, inventore, quasi nisi. Consequuntur, quidem. Sint, dicta?</p>
     </div>
