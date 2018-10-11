4a66e77b34e4852eaf6940ba6fa424ef166bf0e8
jpwmaher <jonmaher@me.com>
2018-10-11 16:09:55 +0100

    mobile menus

--- include-concept-omni-column-2-clinical-gpc.php
+++ include-concept-omni-column-2-clinical-gpc.php
@@ -1,4 +1,4 @@
-<div id="second-column" class="cell medium-2 medium-cell-block-y content-column">
+<div id="second-column" class="cell medium-2 medium-cell-block-y content-column show-for-medium">
     <ul class="menu vertical accordion-menu" data-deep-linking="true" data-accordion-menu data-submenu-toggle="true">
         <li>
             <a id="col-2-link-1" href="<?php echo $link_clinical_cat_1 ?>">
@@ -22,6 +22,52 @@
                 <?php echo $label_clinical_cat_2 ?>
             </a>
             <ul class="menu vertical nested is-active">
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#second">Appointments</a> </li>
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#third">Encounters (Journal)</a> </li>
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#fourth">Clinical Items</a> </li>
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#fifth">Problems</a> </li>
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#sixth">Allergies</a> </li>
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#seventh">Medications</a> </li>
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#eighth">Referrals</a> </li>
+                <li> <a href="<?php echo $link_clinical_cat_2 ?>#ninth">Observations</a> </li>
+            </ul>
+        </li>
+        <li>
+            <a id="col-2-link-3" href="#">
+                Care Plans
+            </a>
+            <ul class="menu vertical nested">
+                <li> <a href="<?php echo $link_clinical_mhcp ?>">Mental Health Crisis Plan</a> </li>
+                <li> <a href="<?php echo $link_clinical_eolp ?>">End of Life</a> </li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
+<div id="second-column" class="cell medium-2 medium-cell-block-y content-column hide-for-medium">
+    <ul class="menu vertical accordion-menu" data-deep-linking="true" data-accordion-menu data-submenu-toggle="true">
+        <li>
+            <a id="col-2-link-1" href="<?php echo $link_clinical_cat_1 ?>">
+                <?php echo $label_clinical_cat_1 ?>
+            </a>
+            <ul class="menu vertical nested">
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#first">Allergies &amp; Sensitivities</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#second">Acute Medications</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#third">Current Repeat Medications</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#fourth">Past Repeat Medications</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#fifth">Diagnoses</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#sixth">Problems &amp; issues</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#seventh">Observations</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#eighth">Treatments</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#ninth">Results</a></li>
+                <li><a href="<?php echo $link_clinical_cat_1 ?>#tenth">Care events</a></li>
+            </ul>
+        </li>
+        <li>
+            <a id="col-2-link-2" href="<?php echo $link_clinical_cat_2 ?>">
+                <?php echo $label_clinical_cat_2 ?>
+            </a>
+            <ul class="menu vertical nested">
                 <li> <a href="<?php echo $link_clinical_cat_2 ?>#second">Appointments</a> </li>
                 <li> <a href="<?php echo $link_clinical_cat_2 ?>#third">Encounters (Journal)</a> </li>
                 <li> <a href="<?php echo $link_clinical_cat_2 ?>#fourth">Clinical Items</a> </li>
