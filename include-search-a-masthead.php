702bc037a3a3c2b4400a948249b90a3d7f12b3bd
jpwmaher <jonmaher@me.com>
2018-10-15 11:44:13 +0100

    Update include-search-a-masthead.php

--- include-search-a-masthead.php
+++ include-search-a-masthead.php
@@ -1,7 +1,7 @@
 <div class="grid-container full">
     <div class="grid-x grid-padding-x grid-margin-x">
         <div class="cell">
-            <img src="img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;">
+            <a href="index.html"><img src="img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;"></a>
             <!-- <a href="#" style="float: right; padding: 5px 0px;">New patient search</a> -->
         </div>
     </div>
