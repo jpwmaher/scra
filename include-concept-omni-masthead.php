b78532d5b163a329b4fd0f5b3adbf82f0132a2c8
jpwmaher <jonmaher@me.com>
2018-10-15 11:26:03 +0100

    Update include-concept-omni-masthead.php

--- include-concept-omni-masthead.php
+++ include-concept-omni-masthead.php
@@ -1,6 +1,6 @@
 <div id="masthead" class="grid-x grid-padding-x">
     <div class="cell">
-        <img src="img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;">
+        <a href="index.html"><img src="img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;"></a>
         <a href="#" style="float: right; padding: 5px 0px;" class="toolbar-button">New patient search</a>
     </div>
 </div>
