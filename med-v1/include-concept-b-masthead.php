6fc910fafc132c70284a00c293030522b5fc9bba
Dan Johnston <daniel.johnston1@nhs.net>
2018-11-26 15:04:56 +0000

    added alt text to header image

--- med-v1/include-concept-b-masthead.php
+++ med-v1/include-concept-b-masthead.php
@@ -1,4 +1,4 @@
 <div class="cell">
-    <a href="../index.html"><img src="../img/nhs-digital-logo-left.jpg" style="height: 30px; padding: 5px 0px;"></a>
+    <a href="../index.html"><img src="../img/nhs-digital-logo-left.jpg" alt="NHS-logo" style="height: 30px; padding: 5px 0px;"></a>
     <a href="#" style="float: right; padding: 5px 0px;">New patient search</a>
 </div>
